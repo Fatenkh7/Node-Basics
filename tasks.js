59a686b193a767f010148a1104a80361618fcfa4
Faten_khoder <Khoderfaten5@gmail.com>
2022-12-22 10:48:35 +0000

    help command

--- tasks.js
+++ tasks.js
@@ -36,6 +36,8 @@ function onDataReceived(text) {
     quit();
   } else if (text === "hello\n") {
     hello();
+  } else if (text === "help\n") {
+    help();
   } else {
     unknownCommand(text);
   }
@@ -69,6 +71,10 @@ function hello() {
 function quit() {
   console.log("Quitting now, goodbye ;) !");
   process.exit();
+}
+/** help */
+function help() {
+  console.log("Commands:\n" + "  quit\n" + "exit\n" + "  hello\n");
 }
 
 // The following line starts the application
